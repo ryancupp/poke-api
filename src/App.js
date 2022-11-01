dc3961036aed1fbb851552819f0e1195e4eac6cf
ryancupp <ryancupp89@gmail.com>
2022-11-01 13:30:07 -0600

    poke api 1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  const fetchPoke = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+    .then(response => {
+      return response.json();
+  }).then(response => {
+      console.log(response);
+      setPokemon(response.results);
+  }).catch(err=>{
+      console.log(err);
+  });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchPoke}>fetch pokemon</button>
+      {
+      pokemon.map((poke, idx) =>{
+        return (
+          <p key={idx}>{poke.name}</p>
+        )
+      })}
+  
     </div>
   );
 }
